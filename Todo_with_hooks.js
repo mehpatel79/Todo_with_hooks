95763bc0e87e970ac781e3160d4437abf85e7503
mehpatel79 <123248785+mehpatel79@users.noreply.github.com>
2023-04-26 18:44:34 +0530

    Stage 1 Complete

--- Todo_with_hooks.js
+++ Todo_with_hooks.js
@@ -5,24 +5,51 @@ import strike from './strike.css';
 const Todo_with_hooks = () => {
 
     const [inputValue, setInputValue] = useState("");
-    const [inputlist, setInputList] = useState([]);
+    const [inputList, setInputList] = useState([]);
     const [displayList1, setDisplayList1] = useState([]);
     const [displayList2, setDisplayList2] = useState([]);
     const [togglebtn, setTogglebtn] = useState(true);
 
+    const handleChange = (e) =>{
+      setInputValue(e.target.value);
+      
+    }
+
     const submitTask = () => {
-          let i = inputlist.length + 1;
+          let i = inputList.length + 1;
           let task = inputValue;
             if (task === "") {alert("Please Enter Task")}
             else {
-                  let addTask = {id:i, titel:task, status:false };
-                  setInputList(addTask);
-                  setDisplayList1(addTask);
+                  let addTask = {id:i, title:task, status:false };
+                  setInputList([...inputList, addTask]);
+                  setDisplayList1([...displayList1, addTask]);
                   setInputValue("");
             }     
       }
 
-    
+    const resetTask = () => {
+      setInputValue("");
+      setInputList([]);
+      setDisplayList1([]);
+      setDisplayList2([]);
+    }
+
+    const chxCheck = (id) => {
+        let checkEntry = displayList1.filter(task => task.id !== id);
+   
+        let checkEntry2 = displayList1.map(task => {
+        if(task.id === id){ 
+          return ({id:task.id, title:task.title, status : true})
+        }
+        return task
+      });
+      checkEntry2 = checkEntry2.filter(task => task.id === id);
+      setInputValue("");
+      setDisplayList1([...checkEntry]);
+      setDisplayList2([...displayList2, ...checkEntry2]);
+      }
+  
+      
   
 
   
@@ -34,22 +61,7 @@ const Todo_with_hooks = () => {
     this.setState({inputValue : "", arlist : [], arlist2: []});
   }
 
-  chxCheck(id){
-    let checkEntry = this.state.arlist.filter(task => task.id !== id);
- 
-    let checkEntry2 = this.state.arlist.map(task => {
-      if(task.id === id){ 
-        return ({id:task.id, title:task.title, status : true})
-      }
-      return task
-    });
-    checkEntry2 = checkEntry2.filter(task => task.id === id);
-    //this.state.arlist = [...checkEntry];
-    //this.state.arlist2 = [...this.state.arlist2, ...checkEntry2];
-    
-    this.setState({inputValue: "", arlist: [...checkEntry], arlist2 : [...this.state.arlist2, ...checkEntry2]});   
-  }
-
+  
   chxCheck2(id){
     let checkEntry21 = this.state.arlist2.map(task => {
       if(task.id === id){ 
@@ -107,11 +119,11 @@ cancel(){
   
     return (
             <>
-              <input type="text" value={inputValue}/>
+              <input type="text" onChange={handleChange} value={inputValue}/>
               {(togglebtn)?
                   <>
-                    <button >Add Task</button>
-                    <button >Reset</button>
+                    <button onClick={submitTask}>Add Task</button>
+                    <button onClick={resetTask}>Reset</button>
                   </>
                 :
                   <>
@@ -119,17 +131,17 @@ cancel(){
                     <button >Cancel</button>
                   </>
               }
-        {/*(this.state.arlist.length !==0)?<h3>To Do List</h3>:<h3></h3>}
+        {(displayList1.length !==0)&& <h3>To Do List</h3>}
         <ul>
-              {this.state.arlist.map((task,index) => {
+              {displayList1.map((task,index) => {
                   return (<div key={task.id}>
-                  <li> {task.title} <input type="checkbox" onClick={(e)=>{this.chxCheck(task.id)}} value={task.title}></input></li>
+                  <li> {task.title} <input type="checkbox" onClick={(e)=>{chxCheck(task.id)}} value={task.title}></input></li>
                   </div>);
                   })}
 
         </ul>
+        {/*(this.state.arlist2.length !==0)?<h3>Completed Task</h3>:<h3></h3>}
         <hr />
-        {(this.state.arlist2.length !==0)?<h3>Completed Task</h3>:<h3></h3>}
         <ul>
               {this.state.arlist2.map((task) => {
                   return (<div key={task.id}>
